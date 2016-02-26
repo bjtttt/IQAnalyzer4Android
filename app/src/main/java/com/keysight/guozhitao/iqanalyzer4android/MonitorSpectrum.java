06e58f44e3309001cbb1e3f36fc2f2b3feac5626
bjtttt <bjguozhitao@icloud.com>
2016-02-26 21:11:14 +0800

    ActionBar Menu Adjust
    Begin

--- app/src/main/java/com/keysight/guozhitao/iqanalyzer4android/MonitorSpectrum.java
+++ app/src/main/java/com/keysight/guozhitao/iqanalyzer4android/MonitorSpectrum.java
@@ -23,6 +23,7 @@ public class MonitorSpectrum extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +32,7 @@ public class MonitorSpectrum extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+        */
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -67,9 +69,11 @@ public class MonitorSpectrum extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        /*
         if (id == R.id.action_settings) {
             return true;
         }
+        */
 
         return super.onOptionsItemSelected(item);
     }
