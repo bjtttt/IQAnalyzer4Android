0885963e1776106201ac65a873db2a07c36b6e00
bjtttt <bjguozhitao@icloud.com>
2016-03-22 20:36:48 +0800

    Add CommonView

---
+++ measurebar/src/main/java/com/keysight/guozhitao/measurebar/MeasureBar.java
@@ -0,0 +1,10 @@
+package com.keysight.guozhitao.measurebar;
+
+import android.app.Fragment;
+import android.view.View;
+
+/**
+ * Created by cn569363 on 3/22/2016.
+ */
+public class MeasureBar extends Fragment {
+}
